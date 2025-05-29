113d48e20510c20e51a9b38e746a9f5159d7843c
Oluwaseyi <oluwaseyi.makinde@decagon.dev>
2025-05-29 18:03:15 +0100

    corrected th query

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340om;
+    OWNER TO cse345;
 
 
 CREATE TABLE public.classification(
